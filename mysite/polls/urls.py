3847e7cb4ede789a20d9c09bc3f2a7204c861742
@4vedi <4vedi.aayush@gmail.com>
2017-11-24 08:31:01 +0530

    Add files via upload

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -3,4 +3,11 @@ from . import views
 
 urlpatterns = [
     url(r'^$',views.index, name="index"),
+    # ex: /polls/
+    url(r'^(?P<question_id>[0-9]+)/$',views.detail, name ="details"),
+    # ex: /polls/5/
+    url(r'^(?P<question_id>[0-9]+)/results$',views.results, name ="results"),
+     # ex: /polls/5/results/
+    url(r'^(?P<question_id>[0-9]+)/votes$',views.votes, name ="votes"),
+    # ex: /polls/5/vote/
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,7 +3,20 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
+from django.template import loader, RequestContext
 
 # Create your views here.
 def index(request):
-    return HttpResponse("The data is shown.Bye")
+    latest_questions = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_questions': latest_questions}
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    return HttpResponse("Details of ques: %s" % question_id)
+
+def results(request, question_id):
+    return HttpResponse("Results of ques: %s" % question_id)
+
+def votes(request, question_id):
+    return HttpResponse("Votes of ques: %s" % question_id)
